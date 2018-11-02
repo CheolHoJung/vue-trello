665637b7ba70d3cc9f8159ba77fad63c451a7e48
CheolHoJung <fnel159@naver.com>
2018-11-02 22:52:59 +0900

    refactor: Add NotFound page component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
 
 const routes = {
   '/': App,
@@ -12,8 +13,7 @@ new Vue({
   el: '#app',
   computed: {
     VueComponent() {
-      return routes[window.location.pathname] ||
-        { template: '<div>Page not found</div>' }
+      return routes[window.location.pathname] || NotFound
     }
   },
   render(h) {
