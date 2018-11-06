55c293644701a4d12627edb6fc3f487a7d92cd39
CheolHoJung <fnel159@naver.com>
2018-11-06 23:08:12 +0900

    refactor: Change to use axios to http request

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,8 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>
-        Api result: {{apiRes}}
-      </div>
+      <div v-else>Api result: {{apiRes}}</div>
+      <div v-if="error">error: <pre>{{error}}</pre></div>
 
       <ul>
         <li>
@@ -21,11 +20,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       loading: false,
-      apiRes: ""
+      apiRes: '',
+      error: ''
     }
   },
   created() {
@@ -35,20 +37,28 @@ export default {
     fetchData() {
       this.loading = true
 
-      const req = new XMLHttpRequest()
+      axios.get('http://localhost:3000/health')
+        .then(res => {
+          this.apiRes = res.data
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading = false;
+        })
 
-      req.open('GET', 'http://localhost:3000/health')
-
-      req.send()
-
-      req.addEventListener('load', () => {
-        this.loading = false;
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      })
+      // error
+      axios.get('http://localhost:3000/_health')
+        .then(res => {
+          this.apiRes = res.body
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading = false;
+        })
     }
   }
 }
