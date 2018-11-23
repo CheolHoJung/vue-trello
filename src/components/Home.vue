a1eb625fd4a2f174768bafcb004f9e63ec625b76
jcheolho <fnel159@naver.com>
2018-11-23 22:38:18 +0900

    refactor: Change api call to use vuex

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
         </router-link>
       </div>
       <div class="board-item board-item-new">
-        <a class="new-board-btn" href="" @click.prevent="addBoard">
+        <a class="new-board-btn" href="" @click.prevent="SET_IS_ADD_BOARD(true)">
           Create new board...
         </a>
       </div>
@@ -21,6 +21,7 @@
 <script>
 import {board} from '../api'
 import AddBoard from './AddBoard.vue'
+import {mapState, mapMutations} from 'vuex'
 
 export default {
   components: {
@@ -30,9 +31,13 @@ export default {
     return {
       loading: false,
       boards: [],
-      error: '',
-      isAddBoard: false
+      error: ''
     }
+  },
+  computed: {
+    ...mapState([
+      'isAddBoard'
+    ])
   },
   created() {
     this.fetchData()
@@ -43,6 +48,9 @@ export default {
     })
   },
   methods: {
+    ...mapMutations([
+      'SET_IS_ADD_BOARD'
+    ]),
     fetchData() {
       this.loading = true
       board.fetch()
@@ -53,12 +61,8 @@ export default {
           this.loading = false;
         })
     },
-    addBoard() {
-      this.isAddBoard = true;
-    },
-    onAddBoard(title) {
-      board.create(title)
-        .then(() => this.fetchData())
+    onAddBoard() {
+      this.fetchData()
     }
   }
 }
