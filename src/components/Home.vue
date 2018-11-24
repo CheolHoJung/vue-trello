4bd9f0d24ca06d7a793dd86a1978a544f6533d4c
jcheolho <fnel159@naver.com>
2018-11-24 16:23:39 +0900

    refactor: Change using mutation and action at board fetch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,14 +14,14 @@
         </a>
       </div>
     </div>
-    <AddBoard v-if="isAddBoard" @close="isAddBoard=false" @submit="onAddBoard" />
+    <AddBoard v-if="isAddBoard" @close="isAddBoard=false" />
   </div>
 </template>
 
 <script>
+import {mapState, mapMutations, mapActions} from 'vuex'
 import {board} from '../api'
 import AddBoard from './AddBoard.vue'
-import {mapState, mapMutations} from 'vuex'
 
 export default {
   components: {
@@ -30,13 +30,13 @@ export default {
   data() {
     return {
       loading: false,
-      boards: [],
       error: ''
     }
   },
   computed: {
     ...mapState([
-      'isAddBoard'
+      'isAddBoard',
+      'boards'
     ])
   },
   created() {
@@ -51,18 +51,15 @@ export default {
     ...mapMutations([
       'SET_IS_ADD_BOARD'
     ]),
+    ...mapActions([
+      'FETCH_BOARDS'
+    ]),
     fetchData() {
       this.loading = true
-      board.fetch()
-        .then(data => {
-          this.boards = data.list;
-        })
+      this.FETCH_BOARDS()
         .finally(() => {
           this.loading = false;
         })
-    },
-    onAddBoard() {
-      this.fetchData()
     }
   }
 }
